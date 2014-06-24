ec45f5f1a54bd3dad420e979ad66d7d64af300ce
Nan Liu <nan.liu@gmail.com>
2014-06-24 15:35:15 -0700

    lint whitespace cleanup.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,14 @@
 class tomcat (
-  $version = '6.0.41',
-  $url = 'http://apache.mirrors.hoobly.com/tomcat',
-  $user = 'tomcat',
-  $group = 'tomcat',
-  $path = '/opt/tomcat',
-  $use_http_only = false,
-  $java_xms = '1536m',
-  $java_xmx = '1536m',
+  $version            = '6.0.41',
+  $url                = 'http://apache.mirrors.hoobly.com/tomcat',
+  $user               = 'tomcat',
+  $group              = 'tomcat',
+  $path               = '/opt/tomcat',
+  $use_http_only      = false,
+  $java_xms           = '1536m',
+  $java_xmx           = '1536m',
   $java_max_perm_size = '256m',
-  $java_options = '-Djava.awt.headless=true',
+  $java_options       = '-Djava.awt.headless = true',
 ) {
   require 'java'
 

8b41f9816b000aa0c89e94db856f311a2092a27a
Nan Liu <nan.liu@gmail.com>
2014-06-24 15:34:58 -0700

    Remove unused parameter.

--- manifests/war.pp
+++ manifests/war.pp
@@ -6,7 +6,6 @@ define tomcat::war (
     group   => $tomcat::group,
     mode    => '0644',
     source  => $source,
-    content => $content,
     require => File[$tomcat::path],
     notify  => Service['tomcat'],
   }

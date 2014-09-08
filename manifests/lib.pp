869709d259b2a1ae2a90050935994ea7b6dbf404
Nan Liu <nan.liu@gmail.com>
2014-09-08 15:00:47 -0700

    Fix issue if user pass content.

--- manifests/lib.pp
+++ manifests/lib.pp
@@ -12,7 +12,7 @@ define tomcat::lib (
     fail("Must provide source or content for tomcat::conf ${name}")
   }
 
-  if $source =~ /^puppet/ or $source =~ /^\// {
+  if $content or $source =~ /^puppet/ or $source =~ /^\// {
     file { $file_path:
       owner   => $::tomcat::user,
       group   => $::tomcat::group,
